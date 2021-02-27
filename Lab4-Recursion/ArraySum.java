f47f97363e103c9acd64136b9012fc9bb11fa15f
Shahdad02 <71472454+Shahdad02@users.noreply.github.com>
2021-02-26 19:22:00 -0500

    Lab 4

---
+++ Lab4-Recursion/ArraySum.java
@@ -0,0 +1,15 @@
+public class ArraySum {
+
+    public int sumOfArray(Integer[] a, int index){
+
+        int sum;
+
+        if(index == 0) {
+            sum = a[index];
+        }else{
+            sum = a[index] + sumOfArray(a, index-1);
+        }
+        return sum;
+    }
+
+}
